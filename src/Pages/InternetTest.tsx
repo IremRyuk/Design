11e200e0fdfcbee30695ee7ed91e5f9fd65ee15c
IremRyuk <IremRyuk>
2023-06-06 20:29:42 +0400

    working speed test function

--- src/Pages/InternetTest.tsx
+++ src/Pages/InternetTest.tsx
@@ -1,11 +1,42 @@
 import { Box, Typography,Stack,Button } from "@mui/material";
-import { ReactInternetSpeedMeter } from 'react-internet-meter'
-import 'react-internet-meter/dist/index.css'
-import {useEffect, useState} from 'react'
+// import {useEffect, useState} from 'react'
 
 export default function InternetTest() {
-  const [stopTest,SetStop] = useState<boolean>(false)
-  const [checkSpeed, SetCheckSpeed] = useState<number>(0);
+  const imageAddr =
+  "http://wallpaperswide.com/download/shadow_of_the_tomb_raider_2018_puzzle_video_game-wallpaper-7680x4800.jpg" +
+  "?n=" +
+  Math.random();
+let startTime: number, endTime: number;
+let downloadSize:number = 5616998; //5.36Mb
+let download = new Image();
+let roundedDecimals:number = 2;
+let bytesInAKilobyte:number = 1024;
+
+function showResults() {
+  let duration = (endTime - startTime) / 1000;
+  let bitsLoaded = downloadSize * 8;
+  let speedBps = (bitsLoaded / duration).toFixed(roundedDecimals);
+  let displaySpeed = speed(speedBps);
+  console.log(displaySpeed.value,displaySpeed.units)
+}
+
+function speed(bitsPerSecond: any) {
+  let KBps:number = parseInt((bitsPerSecond / bytesInAKilobyte).toFixed(roundedDecimals));
+  if (KBps <= 1) return { value: bitsPerSecond, units: "Bps" };
+  var MBps:number = parseInt((KBps / bytesInAKilobyte).toFixed(roundedDecimals));
+  if (MBps <= 1) return { value: KBps, units: "KBps" };
+  else return { value: MBps, units: "MBps" };
+}
+
+function play(){
+  download.onload = function () {
+    endTime = new Date().getTime();
+    showResults();
+  };
+  startTime = new Date().getTime();
+  download.src = imageAddr;
+}
+
   return (
     <Box
     width='100vw'
@@ -24,31 +55,9 @@ p={5}
 spacing={2}
 >
   <Typography variant='h5'>Click On Start Button To Test Your Internet Speed</Typography>
-  <Typography variant='h4'>{checkSpeed},mb</Typography>
-  {/* <Button onClick={()=>play()}>Press To Start</Button> */}
+  <Typography variant='h4'>45,mb</Typography>
+  <Button onClick={()=>play()}>Press To Start</Button>
   </Stack>
-
-
-
-
-      {/* This is Speed Test Form Real-Internet-Meter */}
-      {stopTest && 
-      <>
-      <ReactInternetSpeedMeter
-        txtSubHeading="Internet connection is slow"
-        outputType="" // "alert"/"modal"/"empty"
-        customClassName={null}
-        pingInterval={2000} // milliseconds
-        txtMainHeading="Opps..."
-        thresholdUnit="megabyte" // "byte" , "kilobyte", "megabyte"
-        threshold={50}
-        imageUrl="https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg?cs=srgb&dl=pexels-francesco-ungaro-1525041.jpg&fm=jpg"
-        downloadSize="1561257" //bytes
-        callbackFunctionOnNetworkTest={(data:any) => SetCheckSpeed(data)}
-          />
-      </>
-      }
-
     </Box>
   )
 }
