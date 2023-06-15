792140a9191bf346932602acb3da9ae0f7abef93
IremRyuk <IremRyuk>
2023-06-15 13:31:18 +0400

    change swiper to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,21 @@
 import './Styles/App/app.css'
-import { Swiper,SwiperSlide } from 'swiper/react';
 import SaveData from './Pages/SaveData';
-import 'swiper/css'
-import 'swiper/css/effect-fade'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import { useSelector } from 'react-redux';
-import Nav from './Components/Nav';
-import {useState,useEffect} from 'react'
 import Home from './Pages/Home';
 import Converting from './Pages/Converting';
 import InternetTest from './Pages/InternetTest';
 import Paint from './Pages/Paint';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
-  // Redux
-  let value = useSelector((state:any)=>state.changeValue)
-
-  // State For Changing Swiper Page
-  let [swiper,setSwiper] = useState<any>(null)
-
-  // Change Swiper Page
-useEffect(()=>{
-  if(swiper){
-    swiper.slideTo(value)
-  }
-},[value])
-
   return (
     <div className="App">
-      <Nav />
-      <Swiper
-      initialSlide={value}
-      onSwiper={setSwiper}
-      spaceBetween={0}
-      slidesPerView={1}
-      allowTouchMove={true}
-
-      >
-        <SwiperSlide><InternetTest /></SwiperSlide>
-        <SwiperSlide><Home /></SwiperSlide>        
-        <SwiperSlide><Paint /></SwiperSlide>
-        <SwiperSlide><SaveData /></SwiperSlide>
-        <SwiperSlide><Converting /></SwiperSlide>
-      </Swiper>
+      <Routes>
+        <Route path='/' element={<InternetTest />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Paint />} />
+        <Route path='/' element={<SaveData />} />
+        <Route path='/' element={<Converting />} />
+        </Routes>
     </div>
   );
 }
